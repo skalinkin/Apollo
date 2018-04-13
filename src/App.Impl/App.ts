7480bfad5f721afaaa782b6da0ce23a316f99177
skalinkin <skalinkin@gmail.com>
2018-04-13 11:07:09 -0400

    Progress

--- src/App.Impl/App.ts
+++ src/App.Impl/App.ts
@@ -2,6 +2,7 @@ import {  inject,  injectable,  named} from "inversify";
 import IApp from "../App/IApp";
 import IStatusMonitor from "../App/IStatusMonitor";
 import IServerConfig from "../App/IServerConfig";
+
 var http = require('http');
 
 @injectable()
@@ -10,9 +11,12 @@ class App implements IApp {
   private _server: any;
   private _config: IServerConfig;
 
-  constructor(@inject('IStatusMonitor') monitor: IStatusMonitor, @inject('IServerConfig') config: IServerConfig) {
+  constructor(@inject('IStatusMonitor') monitor: IStatusMonitor
+               , @inject('IServerConfig') config: IServerConfig) {
     this._monitor = monitor;
-    this._server = http.createServer((req, res) => {
+    this._server = http.createServer(
+      //process the request is different responsibility. should be separated
+      (req, res) => {
       res.writeHead(200, {
         'Content-Type': 'text/plain'
       });
