f2a64fe5f6eb968794cfee80f52eef81c6f5bdca
skalinkin <skalinkin@gmail.com>
2018-04-17 08:23:48 -0400

    progress

--- src/App.Impl/App.ts
+++ src/App.Impl/App.ts
@@ -2,6 +2,7 @@ import {  inject,  injectable,  named} from "inversify";
 import IApp from "../App/IApp";
 import IStatusMonitor from "../App/IStatusMonitor";
 import IServerConfig from "../App/IServerConfig";
+import IServerRequestHandler from "../App/IServerRequestHandler";
 
 var http = require('http');
 
@@ -10,23 +11,26 @@ class App implements IApp {
   private _monitor: IStatusMonitor;
   private _server: any;
   private _config: IServerConfig;
+  private _reqHandler: IServerRequestHandler;
 
   constructor(@inject('IStatusMonitor') monitor: IStatusMonitor
-               , @inject('IServerConfig') config: IServerConfig) {
+               , @inject('IServerConfig') config: IServerConfig
+               , @inject('IServerRequestHandler') serverRequestHandler: IServerRequestHandler) {
+    
     this._monitor = monitor;
-    this._server = http.createServer(
-      //process the request is different responsibility. should be separated
-      (req, res) => {
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
-      });
-      res.end('Hello World\n');
-    });
+    this._reqHandler = serverRequestHandler;
+    this._server = http.createServer();
     this._config = config;
   }
+  
   start() {
     var port = this._config.getPort();
+    
     this._server.listen(port, (err) => this._monitor.monitor(err));
+    
+    this._server.on('request', (req, res) => {
+      this._reqHandler.handleRequest(req, res);
+    });
   }
 }
 
--- src/Infrastructure/Installer.ts
+++ src/Infrastructure/Installer.ts
@@ -7,12 +7,14 @@ import IErrorHandler from "../App/IErrorHandler";
 import ILog from "../App/ILog";
 import IServerConfig from "../App/IServerConfig";
 import IStatusMonitor from "../App/IStatusMonitor";
+import IServerRequestHandler from "../App/IServerRequestHandler";
 
 import App from "../App.Impl/App";
 import ErrorHandler from "../App.Impl/ErrorHandler";
 import Log from "../App.Impl/Log";
 import ServerConfig from "../App.Impl/ServerConfig";
 import StatusMonitor from "../App.Impl/StatusMonitor";
+import ServerRequestHandler from "../App.Impl/ServerRequestHandler";
 
 let container = new Container();
 
@@ -21,5 +23,6 @@ container.bind<IErrorHandler>("IErrorHandler").to(ErrorHandler);
 container.bind<ILog>("ILog").to(Log);
 container.bind<IServerConfig>("IServerConfig").to(ServerConfig);
 container.bind<IStatusMonitor>("IStatusMonitor").to(StatusMonitor);
+container.bind<IServerRequestHandler>("IServerRequestHandler").to(ServerRequestHandler);
 
 export default container;
